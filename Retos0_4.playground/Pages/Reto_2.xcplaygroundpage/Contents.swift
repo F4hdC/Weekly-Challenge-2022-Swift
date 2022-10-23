4d855be7a44bc880316dbbb26c2d296afdebbb1b
F4hdC <fhdwebdesign@gmail.com>
2022-10-23 23:13:16 +0200

    Reto 1, números primos

--- Retos0_4.playground/Pages/Reto_2.xcplaygroundpage/Contents.swift
+++ Retos0_4.playground/Pages/Reto_2.xcplaygroundpage/Contents.swift
@@ -1,1 +1,24 @@
 import Foundation
+
+// Reto 1 números primos
+
+
+func esPrimo(num: Int) -> Bool {
+    
+    guard num > 1 else {
+        return false
+    }
+    
+    for i in 2..<num {
+        if num % i != 0 {
+            
+        }else {
+            return false
+        }
+    }
+    return true
+}
+
+for i in 1...100{
+    print(esPrimo(num: i) ? "\(i) " : "", terminator: "")
+}
