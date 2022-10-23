20c27e4be6758f18e809ce65c500eebc0e4dee37
F4hdC <fhdwebdesign@gmail.com>
2022-10-23 23:57:00 +0200

    Reto2,3, números primmos y sucesión de fibonacci

--- Retos0_4.playground/Pages/Reto_2.xcplaygroundpage/Contents.swift
+++ Retos0_4.playground/Pages/Reto_2.xcplaygroundpage/Contents.swift
@@ -1,24 +1,46 @@
 import Foundation
 
-// Reto 1 números primos
+// Reto 2, sucesión de fibonacci
+
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
 
 
-func esPrimo(num: Int) -> Bool {
+func fibonacci(num: Int) -> [Int] {
     
-    guard num > 1 else {
-        return false
+    var sucesión = [Int]()
+    var a = 1, b = 0, c = 0
+    for _ in 0...num {
+        c = b
+        b = a
+        a = b + c
+       sucesión += [c]
     }
-    
-    for i in 2..<num {
-        if num % i != 0 {
-            
-        }else {
-            return false
-        }
-    }
-    return true
+    return sucesión
 }
 
-for i in 1...100{
-    print(esPrimo(num: i) ? "\(i) " : "", terminator: "")
+
+print(fibonacci(num: 20))
+
+
+func fibonacci_(num: Int) -> [Int] {
+    
+    var sucesión = [0,1]
+    var ant_ant = 0, ant = 1, actual = 0
+    for _ in 2...num{
+        actual = ant_ant + ant
+        ant_ant = ant
+        ant = actual
+        
+        
+        sucesión.append(actual)
+    }
+    return sucesión
 }
+
+print(fibonacci_(num: 10))
--- Retos0_4.playground/Pages/Reto_3.xcplaygroundpage/Contents.swift
+++ Retos0_4.playground/Pages/Reto_3.xcplaygroundpage/Contents.swift
@@ -1,2 +1,25 @@
 import Foundation
 
+// Reto 3 números primos
+
+func esPrimo(num: Int) -> Bool {
+    
+    guard num > 1 else {
+        return false
+    }
+    
+    for i in 2..<num {
+        if num % i != 0 {
+            
+        }else {
+            return false
+        }
+    }
+    return true
+}
+
+for i in 1...100{
+    print(esPrimo(num: i) ? "\(i) " : "", terminator: "")
+}
+
+
